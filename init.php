e42d9c3b71c7416093ec5262718c21b139f0b7e4
P Park <paul-github@idealworldinc.com>
2014-05-28 18:07:14 -0400

    Initial commit

---
+++ init.php
@@ -0,0 +1,114 @@
+<?php
+class wallabag extends Plugin {
+
+	private $host;
+
+	function about() {
+		return array(1.0,
+			"Share article on wallabag (formerly poche)",
+			"xppppp");
+	}
+
+	function init($host) {
+		$this->host = $host;
+
+		$host->add_hook($host::HOOK_PREFS_TAB, $this);
+		$host->add_hook($host::HOOK_ARTICLE_BUTTON, $this);
+	}
+	
+	function save() {
+	    $wallabag_url = db_escape_string($_POST["wallabag_url"]);
+	    $wallabag_username = db_escape_string($_POST["wallabag_username"]);
+	    $wallabag_password = db_escape_string($_POST["wallabag_password"]);
+	    $this->host->set($this, "wallabag_url", $wallabag_url);
+	    $this->host->set($this, "wallabag_username", $wallabag_username);
+	    $this->host->set($this, "wallabag_password", $wallabag_password);
+	    echo "Set to post to wallabag at $wallabag_url";
+	}
+
+	function get_js() {
+		return file_get_contents(dirname(__FILE__) . "/wallabag.js");
+	}
+
+	function hook_prefs_tab($args) {
+		 if ($args != "prefPrefs") return;
+
+		 print "<div dojoType=\"dijit.layout.AccordionPane\" title=\"".__("wallabag")."\">";
+
+		 print "<br/>";
+
+		 $w_url = $this->host->get($this, "wallabag_url");
+		 $w_user = $this->host->get($this, "wallabag_username");
+		 $w_pass = $this->host->get($this, "wallabag_password");
+		 print "<form dojoType=\"dijit.form.Form\">";
+
+		 print "<script type=\"dojo/method\" event=\"onSubmit\" args=\"evt\">
+	   evt.preventDefault();
+           if (this.validate()) {
+               console.log(dojo.objectToQuery(this.getValues()));
+               new Ajax.Request('backend.php', {
+                                    parameters: dojo.objectToQuery(this.getValues()),
+                                    onComplete: function(transport) {
+                                         notify_info(transport.responseText);
+                                    }
+                                });
+           }
+           </script>";
+
+		print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"op\" value=\"pluginhandler\">";
+		print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"method\" value=\"save\">";
+		print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"plugin\" value=\"wallabag\">";
+		print "<table width=\"100%\" class=\"prefPrefsList\">";
+		print "<tr><td width=\"40%\">".__("wallabag URL")."</td>";
+		print "<td class=\"prefValue\"><input dojoType=\"dijit.form.ValidationTextBox\" required=\"true\" name=\"wallabag_url\" regExp='^(http|https)://.*' value=\"$w_url\"></td></tr>";
+		print "<tr><td width=\"40%\">".__("wallabag Username")."</td>";
+		print "<td class=\"prefValue\"><input dojoType=\"dijit.form.ValidationTextBox\" name=\"wallabag_username\" regExp='\w{0,64}' value=\"$w_user\"></td></tr>";
+		print "<tr><td width=\"40%\">".__("wallabag Password")."</td>";
+		print "<td class=\"prefValue\"><input dojoType=\"dijit.form.ValidationTextBox\" name=\"wallabag_password\" regExp='.{0,64}' value=\"$w_pass\"></td></tr>";
+		print "</table>";
+		print "<p><button dojoType=\"dijit.form.Button\" type=\"submit\">".__("Save")."</button>";
+
+		print "</form>";
+
+		print "</div>"; #pane
+
+	}
+
+	function hook_article_button($line) {
+		$article_id = $line["id"];
+
+		$rv = "<img id=\"wallabagImgId\" src=\"plugins/wallabag/wallabag.png\"
+			class='tagsPic' style=\"cursor : pointer\"
+			onclick=\"shareArticleTowallabag($article_id)\"
+			title='".__('wallabag')."'>";
+
+		return $rv;
+	}
+
+	function getwallabagInfo() {
+		$id = db_escape_string($_REQUEST['id']);
+
+		$result = db_query("SELECT title, link
+				FROM ttrss_entries, ttrss_user_entries
+				WHERE id = '$id' AND ref_id = id AND owner_uid = " .$_SESSION['uid']);
+
+		if (db_num_rows($result) != 0) {
+			$title = truncate_string(strip_tags(db_fetch_result($result, 0, 'title')),
+				100, '...');
+			$article_link = db_fetch_result($result, 0, 'link');
+		}
+		$wallabag_url = $this->host->get($this, "wallabag_url");
+		$wallabag_username = $this->host->get($this, "wallabag_username");
+		$wallabag_password = $this->host->get($this, "wallabag_password");
+		print json_encode(array("title" => $title, "link" => $article_link,
+				"id" => $id, "wallabag_url" => $wallabag_url,
+				"wallabag_username" => $wallabag_username,
+				"wallabag_password" => $wallabag_password));
+	}
+
+	function api_version() {
+		return 2;
+	}
+
+}
+?>
---
+++ wallabag.js
@@ -0,0 +1,68 @@
+function shareArticleTowallabag(id) {
+    var legacyMethod = function(url, target) {
+	var w = window.open('backend.php?op=backend&method=loading', 'ttrss_tweet',
+			    "status=0,toolbar=0,location=0,width=500,height=400,scrollbars=1,menubar=0");
+	// console.log('post ' + target + ' to ' + url + "?action=add&url=" + btoa(target));
+	w.location.href = url + "?action=add&url=" + btoa(target);
+    };
+    try {
+	new Ajax.Request("backend.php",	{
+	    parameters: {
+		'op': 'pluginhandler',
+		'plugin': 'wallabag',
+		'method': 'getwallabagInfo',
+		'id': param_escape(id)
+	    },
+	    onComplete: function(transport) {
+		var ti = JSON.parse(transport.responseText);
+		
+		if (ti.wallabag_url && ti.wallabag_url.length) {
+		    if (ti.wallabag_username && ti.wallabag_username.length &&
+			ti.wallabag_password && ti.wallabag_password.length) {
+			new Ajax.Request(ti.wallabag_url + "api.php", {
+			    parameters: {
+				'action': 'add',
+				'login': ti.wallabag_username,
+				'password': ti.wallabag_password,
+				'url': btoa(ti.link.strip())
+			    },
+			    method: 'post',
+			    // here comes the CORS fix
+			    // courtesy of: http://stackoverflow.com/questions/13814739/prototype-ajax-request-being-sent-as-options-rather-than-get-results-in-501-err/15300045#15300045
+			    onCreate: function(response) { 
+				var t = response.transport; 
+				t.setRequestHeader = t.setRequestHeader.wrap(function(original, k, v) { 
+				    if (/^(accept|accept-language|content-language)$/i.test(k)) 
+					return original(k, v); 
+				    if (/^content-type$/i.test(k) && 
+					/^(application\/x-www-form-urlencoded|multipart\/form-data|text\/plain)(;.+)?$/i.test(v)) 
+					return original(k, v); 
+				    return; 
+				}); 
+			    },
+			    onSuccess: function (response) {
+				var rObj = JSON.parse(response.responseText);
+				if (rObj.status == 0) {
+				    notify_info("wallabag shared " +
+						rObj.message, false);
+				} else {
+				    notify_error("wallabag post failed " +
+						 rObj.message, true);
+				}
+			    },
+			    onFailure: function (response) {
+				legacyMethod(ti.wallabag_url,
+					     ti.link.strip());
+			    },
+			});
+		    } else {
+			legacyMethod(ti.wallabag_url, ti.link.strip());
+		    }
+		} else {
+		    notify_error("Need to configure wallabag URL", true);
+		}
+	    } });
+    } catch (e) {
+	exception_error("tweetArticle", e);
+    }
+}
